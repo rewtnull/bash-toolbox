9777f61365c2aed8730023bc5463c2c8e40f131a
rewtnull <marcus.hoffren@gmail.com>
2013-03-13 23:42:24 +0100

    Added max.sh

--- max.sh
+++ max.sh
@@ -9,7 +9,7 @@ dates=( 1998-02-12 2003-01-24 2013-03-24 2120-11-11 2010-10-14 2001-11-01 1993-1
 # max(${array[@]} => $largest)
 max() {
     local dates=( "$@" )
-    if (( "${#dates[@]}" > "1" )); then # If array consists of one single element it's already the largest one
+    if (( ${#dates[@]} > "1" )); then # If array consists of one single element it's already the largest one
 	for (( i=0; i < ${#dates[@]}; i++ )); do
 	    gtoe "${dates[$i]}" "${largest}" && largest="${dates[$i]}"
 	done
